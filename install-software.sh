d105f7f43836c387a97d70e6ec1edfb6897f2a2f
Paul Mattern <75864135+pemattern@users.noreply.github.com>
2025-06-12 14:22:56 +0200

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Rust toolchain setup
 # install rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
+# source new envs
+source ~/.cargo/env
 # add rust-analyzer
-rustup
+rustup component add rust-analyzer
+# add wasm target
+rustup target add wasm32-unknown-unknown
+
+# Node.js setup
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+# source new envs
+source ~/.bashrc
+# install latest node version
+nvm install node
